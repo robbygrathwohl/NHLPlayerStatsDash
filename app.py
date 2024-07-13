6d5d9a5578e52dd2cb24a79fc8e45706e49695ba
Robert Grathwohl <robbygrathwohl@gmail.com>
2024-07-12 20:11:45 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,152 @@
+from dash import Dash, html, dcc, Input, Output, Patch, clientside_callback, callback
+import plotly.express as px
+import plotly.io as pio
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
+from utilities import load_data, format_stat_name, create_tab_content, create_player_callback
+from config import skater_stats, teams_color
+
+# Load data with error handling
+file_path = 'data/skaters.csv'
+df = load_data(file_path)
+
+# Initialize dash app with bootstrap theme
+load_figure_template(['minty','minty_dark'])
+#load_figure_template('darkly')
+app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.MINTY, dbc.icons.FONT_AWESOME])
+
+
+
+
+#color switcher
+color_mode_switch =  html.Span(
+    [
+        dbc.Label(className="fa fa-moon", html_for="color-mode-switch"),
+        #dbc.Switch( id="color-mode-switch", value=True, className="d-inline-block ms-1", persistence=True),
+        dbc.Switch( id="color-mode-switch", value=True, className="d-inline-block ms-1", persistence=True),
+        dbc.Label(className="fa fa-sun", html_for="color-mode-switch"),
+    ]
+)
+
+
+
+
+
+# Filter data for each position
+df_c = df[df['position'] == 'C']
+df_lw = df[df['position'] == 'L']
+df_rw = df[df['position'] == 'R']
+df_d = df[df['position'] == 'D']
+
+
+
+
+# Filter data for each position and select top 10 based on key stats
+top_a = df.groupby('name').sum().reset_index().nlargest(50, 'I_F_points')['name'].tolist()
+top_c = df_c.groupby('name').sum().reset_index().nlargest(50, 'I_F_points')['name'].tolist()
+top_rw = df_rw.groupby('name').sum().reset_index().nlargest(50, 'I_F_points')['name'].tolist()
+top_lw = df_lw.groupby('name').sum().reset_index().nlargest(50, 'I_F_points')['name'].tolist()
+top_d = df_d.groupby('name').sum().reset_index().nlargest(50, 'I_F_points')['name'].tolist()
+
+
+
+
+# Create HTML Components
+app.layout = dbc.Container([html.Div([
+    color_mode_switch,
+    # Dashboard section
+    html.Div([
+        html.H1(f'NHL Player Stats 2023-2024', className='text-center'),
+        html.Div([
+            html.Div([
+                html.H2('Players Stats by Position', className='text-center'),
+                dbc.Tabs(
+                    id='position-tabs',
+                    active_tab='C',
+                    class_name='d-flex justify-content-center w-100',
+                    children=[
+                        dbc.Tab(
+                            label='Centers',
+                            tab_id='C',
+                            children=create_tab_content(app, 'C', skater_stats, top_c, df_c)
+                        ),
+                        dbc.Tab(
+                            label='Right Wingers',
+                            tab_id='RW',
+                            children=create_tab_content(app, 'RW', skater_stats, top_rw, df_rw)
+                        ),
+                        dbc.Tab(
+                            label='Left Wingers',
+                            tab_id='LW',
+                            children=create_tab_content(app, 'LW', skater_stats, top_lw, df_lw)
+                        ),
+                        dbc.Tab(
+                            label='Defenseman',
+                            tab_id='D',
+                            children=create_tab_content(app, 'D', skater_stats, top_d, df_d)
+                        ),
+                        dbc.Tab(
+                            label='All Skaters',
+                            tab_id='A',
+                            children=create_tab_content(app, 'All Skaters', skater_stats, top_a, df)
+                        )
+                ])
+            ], className='col-12 col-xl-12 px-12', style={'textAlign': 'center'}),
+        ], className='row')
+    ]),
+    html.Footer([
+        html.Div([
+            html.A('Author: Robby G', href='https://github.com/robbygrathwohl'),
+            html.Span('    |    '),
+            html.A('Dataset Source - MoneyPuck', href='https://moneypuck.com/moneypuck/playerData/seasonSummary/2023/regular/skaters.csv')
+        ], className='bg-dark text-light text-center py-3 fs-5')
+    ])
+])])
+
+
+
+# Player stats by position line charts callback
+create_player_callback(app, 'C', df_c)
+create_player_callback(app, 'RW', df_rw)
+create_player_callback(app, 'LW', df_lw)
+create_player_callback(app, 'D', df_d)
+create_player_callback(app, 'All Skaters', df)
+
+
+
+# @callback(
+#     Output("c-chart", "figure"),
+#     Output("lw-chart", "figure"),
+#     Output("rw-chart", "figure"),
+#     Output("d-chart", "figure"),
+#     Output("all skaters-chart", "figure"),
+#     #Output("switch", "id"),
+#     Input("color-mode-switch", "value"),
+# )
+# def update_figure_template(switch_on):
+#     # When using Patch() to update the figure template, you must use the figure template dict
+#     # from plotly.io  and not just the template name
+#     template = pio.templates["minty"] if switch_on else pio.templates["minty_dark"]
+
+#     patched_figure = Patch()
+#     patched_figure["layout"]["template"] = template
+#     return patched_figure
+
+
+
+clientside_callback(
+    """
+    (switchOn) => {
+       document.documentElement.setAttribute('data-bs-theme', switchOn ? 'light' : 'dark');  
+       return window.dash_clientside.no_update
+    }
+    """
+    ,
+    Output("color-mode-switch", "id"),
+    Input("color-mode-switch", "value"),
+)
+
+
+
+if __name__ == '__main__':
+    app.run_server(port=8080, debug=False)
---
+++ utilities.py
@@ -0,0 +1,124 @@
+from dash import html, dcc, Input, Output, Patch
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.io as pio
+import plotly.express as px
+import plotly.graph_objects as go
+from config import teams_color
+
+
+def load_data(file_path):
+    """
+    Load data from a CSV file with error handling.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        pd.DataFrame: The loaded DataFrame.
+
+    Raises:
+        Exception: If the file is not found, empty, or cannot be parsed.
+    """
+    try:
+        df = pd.read_csv(file_path)
+    except FileNotFoundError:
+        raise Exception(f"The data file '{file_path}' was not found.")
+    except pd.errors.EmptyDataError:
+        raise Exception('The data file is empty.')
+    except pd.errors.ParserError:
+        raise Exception('Error parsing the data file.')
+    return df
+
+def format_stat_name(stat_name):
+    """
+    Format a statistic name to be more readable.
+
+    Args:
+        stat_name (str): The statistic name in snake_case.
+
+    Returns:
+        str: The formatted statistic name.
+    """
+    return ' '.join(word.capitalize() for word in stat_name.split('_'))
+
+def create_tab_content(app, position, stats, top_players, df):
+    """
+    Create the HTML content for each position tab.
+
+    Args:
+        position (str): The position (e.g., 'C', 'RW', 'LW', 'D', 'All Skaters').
+        stats (list): List of statistics to display.
+        top_players (list): List of top players' names.
+        df (pd.DataFrame): DataFrame containing the data for the position.
+
+    Returns:
+        html.Div: The HTML content for the position tab.
+    """
+
+    return html.Div([
+        dcc.Graph(id=f'{position.lower()}-chart'),
+        dcc.Dropdown(
+            id=f'{position.lower()}-stat-dropdown',
+            options=[{'label': format_stat_name(stat), 'value': stat} for stat in stats],
+            value=stats[0],
+            className='btn w-50 mb-4'
+        ),
+        dcc.Dropdown(
+            id=f'{position.lower()}-player-dropdown',
+            options=[{'label': player, 'value': player} for player in df['name'].unique()],
+            value=top_players,
+            multi=True,
+            className='mb-3'
+        )
+    ], className="dash-bootstrap")
+
+def create_player_callback(app, position, df):
+    """
+    Create a callback for updating player charts based on the selected stat and player(s).
+
+    Args:
+        app (Dash): The Dash app instance.
+        position (str): The position (e.g., 'C', 'RW', 'LW', 'D').
+        df (pd.DataFrame): DataFrame containing the data for the position.
+
+    Returns:
+        function: The callback function for updating the chart.
+    """
+    @app.callback(
+        Output(f'{position.lower()}-chart', 'figure'),
+        [Input(f'{position.lower()}-stat-dropdown', 'value'),
+         Input(f'{position.lower()}-player-dropdown', 'value'),
+         Input("color-mode-switch", "value")]
+    )
+    def update_chart(selected_stat, selected_players, switch_on):
+        """assets
+        Update the player chart based on the selected stat and player(s).
+
+        Args:
+            selected_stat (str): The selected statistic to display.
+            selected_players (list): The selected players' names.
+
+        Returns:
+            plotly.graph_objs._figure.Figure: The updated line chart figure.
+        """
+        dbc.Label(className="Player_Stats_Scatter", html_for="scatter")
+        filtered_df = df[df['name'].isin(selected_players)]
+        filtered_df = filtered_df[filtered_df['situation']=='all']
+        hover_template = '<b>%{meta}</b>' + '<br>Icetime : %{x} min'+ '<br>' + selected_stat + ' : %{y}'
+        #template = pio.templates["minty"] if switch_on else pio.templates["minty_dark"]
+        template = pio.templates["minty"]
+    
+
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(meta=filtered_df.name, x=filtered_df.icetime/60, y=filtered_df[selected_stat], mode='markers', marker_color=filtered_df['team'].map(teams_color)))
+        fig.update_layout(title=f'{position} {format_stat_name(selected_stat)}', plot_bgcolor= '#343A40', paper_bgcolor= '#2B3035', font_color= '#eee')
+        fig.update_traces(hovertemplate = hover_template)
+
+        fig.update_traces(marker_line_width=1, marker_size=10)
+        fig.update_yaxes(title_text=format_stat_name(selected_stat))
+        fig.update_xaxes(title_text='Icetime (minutes)')
+        fig["layout"]["template"] = template
+        
+        return fig
+    return update_chart
